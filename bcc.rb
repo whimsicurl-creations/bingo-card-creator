0be814a649043a982345f7ed200e62ae8bdcbd3d
Bethany <blmeier@gmail.com>
2020-09-10 15:58:40 -0400

    Get random BINGO numbers

--- bcc.rb
+++ bcc.rb
@@ -13,3 +13,19 @@ bingo_card = <<-BINGO
 BINGO
 
 puts bingo_card
+
+b_col = (1..15).to_a.sample(5)
+
+i_col = (16..30).to_a.sample(5)
+
+n_col = (31..45).to_a.sample(4)
+
+g_col = (46..60).to_a.sample(5)
+
+o_col = (60..75).to_a.sample(5)
+
+puts b_col.inspect
+puts i_col.inspect
+puts n_col.inspect
+puts g_col.inspect
+puts o_col.inspect
